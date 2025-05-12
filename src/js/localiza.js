654500adfab186cdd41bb42b93c75ebb37f1d02d
Ronaldo <rronaldo.monteiro.almeida@gmail.com>
2025-05-12 09:50:09 -0300

    Adicionando marcadores ao mapa

--- src/js/localiza.js
+++ src/js/localiza.js
@@ -4,23 +4,23 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-//CONTRUINDO A APLICAÇÃO DO MAP DA API
+//CONSTRUINDO A APLICAÇÃO DO MAP DA API
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 
-map.on('click',mapa);
+map.on('click', mapa);
 
-function mapa(e){
-    let lat = e.latlgn.lat.tofixed(6);
-    let long = e.latlgn.lgn.tofixed(6);
-    const locationInfo =document.getElementById('localizacao');
-    if(locationInfo){
-        locationInfo.textContent =` Localização clicada: Latitude ${lat}, Longitude ${long}`; 
+function mapa(e) {
+    let lat = e.latlng.lat.toFixed(6);
+    let long = e.latlng.lng.toFixed(6); // Correção: 'long' para 'lng'
+    const locationInfo = document.getElementById('localizacao'); // Exemplo de como obter a referência ao elemento
+    if (locationInfo) {
+        locationInfo.textContent = `Localização clicada: Latitude ${lat}, Longitude ${long}`;
     }
-    L.marker(e.latlgn).addTo(map)
-    .bindPopup(`Latitude: ${lat}<br> Longitude: ${long}`).openPopup();
+    L.marker(e.latlng).addTo(map)
+        .bindPopup(`Latitude: ${lat}<br>Longitude: ${long}`).openPopup();
 }
 
-map.on('click',mapa);
+	map.on('click', mapa);
